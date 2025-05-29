e7b762a23007b5e31756b61eaaf330611f32904e
Your Name <khalidsaifullaha3@gmail.com>
2025-05-29 14:48:04 +0600

    unuse elimet remove

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import React, { useState } from "react";
 import Home from "@/components/Home/Home";
 import Nav from "@/components/Navbar/Nav";
 
 const HomePage = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
   const openNav = () => {};
 
   return (
